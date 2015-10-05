3bcb02fee59f343d3ba4d21bcf4999397414b946
Yu Feng <yfeng@boingo.com>
2015-10-05 15:34:21 -0700

    A  trailing_zeros.cpp

---
+++ trailing_zeros.cpp
@@ -0,0 +1,21 @@
+/*
+Write an algorithm which computes the number of trailing zeros in n factorial.
+
+Have you met this question in a real interview? Yes
+Example
+11! = 39916800, so the out should be 2
+
+Challenge
+O(log N) time
+*/
+
+#include "header.h"
+
+long long trailingZeros(long long n) {
+
+}
+
+int main(){
+    cout<<trailingZeros(11)<<endl;
+    return 0;
+}
