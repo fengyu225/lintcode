26ee9bd7aeacaa2e2e92d78b0a8dfc1e721173ea
Yu Feng <yfeng@boingo.com>
2015-10-05 15:44:36 -0700

    A  digit_counts.cpp

---
+++ digit_counts.cpp
@@ -0,0 +1,18 @@
+/*
+Count the number of k's between 0 and n. k can be 0 - 9.
+
+Have you met this question in a real interview? Yes
+Example
+if n=12, k=1 in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], we have FIVE 1's (1, 10, 11, 12)
+*/
+
+#include "header.h"
+
+int digitCounts(int k, int n) {
+
+}
+
+int main(){
+    cout<<digitCounts(1, 12)<<endl;
+    return 0;
+}
