1c4870d36b04eb672c418508549a7c0c435fc183
Yu Feng <yfeng@boingo.com>
2015-10-05 15:43:53 -0700

    M  trailing_zeros.cpp

--- trailing_zeros.cpp
+++ trailing_zeros.cpp
@@ -12,10 +12,18 @@ O(log N) time
 #include "header.h"
 
 long long trailingZeros(long long n) {
-     
+    long long res = 0;
+    while(n>=5){
+        res += n/5;
+        n = n/5;
+    }
+    return res;
 }
 
 int main(){
     cout<<trailingZeros(11)<<endl;
+    cout<<trailingZeros(26)<<endl;
+    cout<<trailingZeros(30)<<endl;
+    cout<<trailingZeros(5555550000000)<<endl;
     return 0;
 }
