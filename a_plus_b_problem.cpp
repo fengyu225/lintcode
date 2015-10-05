fd04d780740602c83e8a1ee13f1935afe298383c
Yu Feng <yfeng@boingo.com>
2015-10-05 13:34:47 -0700

    A  a_plus_b_problem.cpp

---
+++ a_plus_b_problem.cpp
@@ -0,0 +1,32 @@
+/*
+Write a function that add two numbers A and B. You should not use + or any arithmetic operators.
+
+Have you met this question in a real interview? Yes
+Example
+Given a=1 and b=2 return 3
+
+Note
+There is no need to read data from standard input stream. Both parameters are given in function aplusb, you job is to calculate the sum and return it.
+
+Challenge
+Of course you can just return a + b to get accepted. But Can you challenge not do it like that?
+
+Clarification
+Are a and b both 32-bit integers?
+
+Yes.
+Can I use bit operation?
+
+Sure you can.
+*/
+
+#include "header.h"
+
+int aplusb(int a, int b) {
+
+}
+
+int main(){
+    cout<<aplusb(1, 2)<<endl;
+    return 0;
+}
