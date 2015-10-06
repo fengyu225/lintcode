4459ed85e4a9c4bfd4393b91d842c25e9942e2ca
Yu Feng <yfeng@boingo.com>
2015-10-05 18:38:32 -0700

    M  digit_counts.cpp

--- digit_counts.cpp
+++ digit_counts.cpp
@@ -9,7 +9,8 @@ if n=12, k=1 in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], we have FIVE 1's (1,
 #include "header.h"
 
 int digitCounts(int k, int n) {
-
+    if(n<=0) return 0;
+    
 }
 
 int main(){
