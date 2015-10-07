2152a454ab96859cb892c1ceb52cd9589155986b
Yu Feng <yfeng@boingo.com>
2015-10-07 16:36:53 -0700

    A  merge_sorted_array_ii.cpp

---
+++ merge_sorted_array_ii.cpp
@@ -0,0 +1,26 @@
+/*
+Merge two given sorted integer array A and B into a new sorted integer array.
+
+Have you met this question in a real interview? Yes
+Example
+A=[1,2,3,4]
+
+B=[2,4,5,6]
+
+return [1,2,2,3,4,4,5,6]
+
+Challenge
+How can you optimize your algorithm if one array is very large and the other is very small?
+*/
+
+#include "header.h"
+
+vector<int> mergeSortedArray(vector<int> &A, vector<int> &B) {
+}
+
+int main(){
+    vector<int> A = {1, 2, 3, 4};
+    vector<int> B = {2, 4, 5, 6};
+    print_vector<int>(mergeSortedArray(A,B));
+    return 0;
+}
