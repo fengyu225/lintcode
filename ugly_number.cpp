04d9e17060819cc35a0b1461aa3a03d988e10e9b
Yu Feng <yfeng@boingo.com>
2015-10-07 14:22:46 -0700

    M  ugly_number.cpp

--- ugly_number.cpp
+++ ugly_number.cpp
@@ -14,6 +14,24 @@ O(K log K) or O(K) time.
 #include "header.h"
 
 long long kthPrimeNumber(int k) {
+    if(k == 1) return 3;
+    deque<long long> q;
+    q.push_back(1);
+    int curr = 1;
+    long long three = 0, five = 0, seven = 0;
+    while(curr<=k){
+        long temp = min(q[three]*3, min(q[five]*5, q[seven]*7));
+        q.push_back(temp);
+        if(temp%3 == 0) three++;
+        if(temp%5 == 0) five++;
+        if(temp%7 == 0){
+            q.pop_front();
+            three--;
+            five--;
+        }
+        curr++;
+    }
+    return q.back();
 }
 
 int main(){
