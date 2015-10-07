077c7cc7c4a73fd5c95b4be08fd12cf2033e9778
Yu Feng <yfeng@boingo.com>
2015-10-07 12:50:57 -0700

    M  digit_counts.cpp

--- digit_counts.cpp
+++ digit_counts.cpp
@@ -10,10 +10,17 @@ if n=12, k=1 in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], we have FIVE 1's (1,
 
 int digitCounts(int k, int n) {
     if(n<=0) return 0;
-    
+    if(n<10) return 1;
+    int y = 1, temp = n;
+    for(;temp>=10;y*=10, temp/=10);
+    if(n/y == k) return n-n/y*y+1+n/y*digitCounts(k, y-1)+digitCounts(k, n-n/y*y);
+    else if(n/y<k) return n/y*digitCounts(k, y-1)+digitCounts(k, n-n/y*y);
+    else // n/y>k
+        return (k==0?0:y)+n/y*digitCounts(k, y-1)+digitCounts(k, n-n/y*y);
 }
 
 int main(){
-    cout<<digitCounts(1, 12)<<endl;
+//    cout<<digitCounts(1, 12)<<endl;
+    cout<<digitCounts(0, 12)<<endl;
     return 0;
 }
