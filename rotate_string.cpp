89bdee3de242a83bd0cce8d6cf91729fbdb7a570
Yu Feng <yfeng@boingo.com>
2015-10-07 17:56:28 -0700

    A  rotate_string.cpp

---
+++ rotate_string.cpp
@@ -0,0 +1,27 @@
+/*
+Given a string and an offset, rotate string by offset. (rotate from left to right)
+
+
+Example
+Given "abcdefg".
+
+offset=0 => "abcdefg"
+offset=1 => "gabcdef"
+offset=2 => "fgabcde"
+offset=3 => "efgabcd"
+Challenge
+Rotate in-place with O(1) extra memory.
+*/
+
+#include "header.h"
+
+void rotateString(string &str,int offset){
+
+}
+
+int main(){
+    string str = "abcdefg";
+    rotateString(str, 3);
+    cout<<str<<endl;
+    return 0;
+}
