97046a690c91b9322af14306cb77c97698b77583
Yu Feng <yfeng@boingo.com>
2016-02-22 01:47:22 -0800

    M  lib.h D  segmeng_tree_build_ii.cpp

--- lib.h
+++ lib.h
@@ -13,6 +13,16 @@ class ListNode {
         ListNode(int x) : val(x), next(NULL) {}
 }; 
 
+class SegmentTreeNode {
+    public:
+        int max;
+        int start, end;
+        SegmentTreeNode* left, *right;
+        SegmentTreeNode(int s, int e):max(0),start(s),end(e),left(NULL),right(NULL) {}
+};
+
+void printSegmentTree(SegmentTreeNode* root);
+
 ListNode* create_list(int arr[], int n);
 ListNode* create_list(vector<int> nums);
 
