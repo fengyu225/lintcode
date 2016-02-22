99e188afc2df6302d08c2c9486b9b7dde138711e
Yu Feng <yfeng@boingo.com>
2016-02-22 15:04:44 -0800

    A  interval_minimum_number.cpp

---
+++ interval_minimum_number.cpp
@@ -0,0 +1,22 @@
+/*
+Given an integer array (index from 0 to n-1, where n is the size of this array), and an query list. Each query has two integers [start, end]. For each query, calculate the minimum number between index start and end in the given array, return the result list.
+
+Example
+For array [1,2,7,8,5], and queries [(1,2),(0,4),(2,4)], return [2,1,5]
+
+Note
+We suggest you finish problem Segment Tree Build, Segment Tree Query and Segment Tree Modify first.
+
+Challenge
+O(logN) time for each query
+*/
+
+#include "header.h"
+
+vector<int> intervalMinNumber(vector<int> &A, vector<Interval> &queries){
+
+}
+
+int main(){
+    return 0;
+}
