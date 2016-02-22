c7e8244679cf95b2bd4224c8285e6cf8ffa8248f
Yu Feng <yfeng@boingo.com>
2016-02-22 02:14:48 -0800

    M  segment_tree_query.cpp

--- segment_tree_query.cpp
+++ segment_tree_query.cpp
@@ -27,10 +27,42 @@ It is much easier to understand this problem if you finished Segment Tree Build 
 
 #include "header.h"
 
+SegmentTreeNode* build_helper(int start, int end, vector<int>& A){
+    if(start>end) return NULL;
+    SegmentTreeNode* root = new SegmentTreeNode(start, end);
+    if(start == end){
+        root->max = A[start];
+        return root;
+    }
+    int m = start+(end-start)/2;
+    SegmentTreeNode* l = build_helper(start, m, A);
+    SegmentTreeNode* r = build_helper(m+1, end, A);
+    root->left = l;
+    root->right = r;
+    root->max = max(l->max, r->max);
+    return root;
+}
+
+SegmentTreeNode * build(vector<int>& A) {
+    int sz = A.size();
+    if(sz == 0) return NULL;
+    return build_helper(0, sz-1, A);
+}
+
 int query(SegmentTreeNode *root, int start, int end) {
-    // write your code here
+    if(start<=root->start && end>=root->end) return root->max;
+    int m = root->start+(root->end-root->start)/2;
+    if(start>m) return query(root->right, start, end);
+    if(end<=m) return query(root->left, start, end);
+    return max(query(root->left, start, m), query(root->right, m+1, end));
 }
 
 int main(){
+    vector<int> nums = {1, 4, 2, 3};
+    SegmentTreeNode* root = build(nums);
+    cout<<query(root, 1, 1)<<endl;
+    cout<<query(root, 1, 2)<<endl;
+    cout<<query(root, 2, 3)<<endl;
+    cout<<query(root, 0, 2)<<endl;
     return 0;
 }
