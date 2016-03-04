c8a5a55d7178641cd091bfa852e402339338f413
Yu Feng <yfeng@boingo.com>
2016-03-03 16:57:58 -0800

    M  graph_valid_tree.cpp A  surrounded_regions.cpp

--- graph_valid_tree.cpp
+++ graph_valid_tree.cpp
@@ -10,8 +10,34 @@ Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
 
 #include "header.h"
 
-bool validTree(int n, vector<vector<int> >& edges) {
+int f(vector<int>& parent, int i){
+    if(parent[i] == -1) return i;
+    int j = i;
+    for(;parent[j] != -1;j = parent[j]);
+    while(parent[i] != -1){
+        int temp = parent[i];
+        parent[i] = j;
+        i = temp;
+    }
+    return j;
+}
 
+bool u(vector<int>& parent, int i, int j){
+    int i_p = f(parent, i);
+    int j_p = f(parent, j);
+    if(i_p == j_p && i_p != -1) return false;
+    parent[i_p] = j_p;
+    return true;
+}
+
+bool validTree(int n, vector<vector<int> >& edges) {
+    int c = edges.size();
+    if(n != c+1) return false;
+    vector<int> parent(n, -1);
+    for(int i=0; i<n-1; i++){
+        if(!u(parent, edges[i][0], edges[i][1])) return false; 
+    }
+    return true;
 }
 
 int main(){
@@ -20,7 +46,7 @@ int main(){
         {0, 2},
         {0, 3},
         {1, 4}
-    }
+    };
     cout<<validTree(5, edges)<<endl;
     return 0;
 }
---
+++ surrounded_regions.cpp
@@ -0,0 +1,30 @@
+/*
+Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O''s into 'X''s in that surrounded region.
+
+X X X X
+X O O X
+X X O X
+X O X X
+
+After capture all regions surrounded by 'X', the board should be:
+
+X X X X
+X X X X
+X X X X
+X O X X
+*/
+
+#include "header.h"
+
+void surroundedRegions(vector<vector<char> >& board) {
+
+}
+
+int main(){
+    vector<vector<char> > board = {
+    
+    };
+    return 0;
+}
