bd923fccaf46e6ee04a1ef42395f458c894f3df4
Yu Feng <yufeng@linkedin.com>
2016-08-05 11:00:44 -0700

    M  subarray_sum.cpp

--- subarray_sum.cpp
+++ subarray_sum.cpp
@@ -6,7 +6,6 @@ Given an integer array, find a subarray where the sum of numbers is zero. Your c
 
 There is at least one subarray that it's sum equals to zero.
 
-Have you met this question in a real interview? Yes
 Example
 Given [-3, 1, 2, -3, 4], return [0, 2] or [1, 3].
 */
@@ -14,7 +13,27 @@ Given [-3, 1, 2, -3, 4], return [0, 2] or [1, 3].
 #include "header.h"
 
 vector<int> subarraySum(vector<int> nums){
-
+    int sz = nums.size();
+    unordered_map<int,int> m;
+    vector<int> arr(sz, 0);
+    arr[0] = nums[0];
+    vector<int> res(2, 0);
+    if(arr[0] == 0) return res;
+    m[arr[0]] = 0;
+    for(int i=1; i<sz; i++){
+        arr[i] = arr[i-1]+nums[i];
+        if(m.find(arr[i]) != m.end()){
+            res[0] = m[arr[i]]+1;
+            res[1] = i;
+            break;
+        }
+        else if(arr[i] == 0){
+            res[1] = i;
+            break;
+        }
+        m[arr[i]] = i;
+    }
+    return res;
 }
 
 int main(){
