b151a215ee63d2140bedc07c01d88489c27c790b
Yu Feng <yufeng@linkedin.com>
2016-08-05 16:04:18 -0700

    M  3_sum_closest.cpp

--- 3_sum_closest.cpp
+++ 3_sum_closest.cpp
@@ -5,7 +5,6 @@ Given an array S of n integers, find three integers in S such that the sum is cl
 
 You may assume that each input would have exactly one solution.
 
-Have you met this question in a real interview? Yes
 Example
 For example, given array S = [-1 2 1 -4], and target = 1. The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 */
@@ -13,7 +12,21 @@ For example, given array S = [-1 2 1 -4], and target = 1. The sum that is closes
 #include "header.h"
 
 int threeSumClosest(vector<int> nums, int target) {
-
+    int sz = nums.size();
+    sort(nums.begin(), nums.end());
+    int res = INT_MAX;
+    for(int i=0; i+2<sz; i++){
+        int temp_t = target-nums[i];
+        int j = i+1;
+        int k = sz-1;
+        while(j<k){
+            int t = nums[j]+nums[k];
+            if(abs(t+nums[i]-target)<abs(res-target)) res = t+nums[i];
+            if(t<temp_t) j++;
+            else k--;
+        }
+    }
+    return res;
 }
 
 int main(){
