b9ef85c6622882ffc132b402b7fbf3c58dcf64a7
Yu Feng <yufeng@linkedin.com>
2016-08-06 18:34:37 -0700

    M  remove_duplicates_from_sorted_arr_ii.cpp

--- remove_duplicates_from_sorted_arr_ii.cpp
+++ remove_duplicates_from_sorted_arr_ii.cpp
@@ -12,11 +12,24 @@ Your function should return length = 5, and A is now [1,1,2,2,3].
 
 int removeDuplicates(vector<int> &nums) {
     int sz = nums.size();
-
+    int index = -1;
+    int count = 1;
+    for(int curr=0; curr<sz; curr++){
+        if(index == -1 || nums[index] != nums[curr]){
+            nums[++index] = nums[curr];
+            count = 1;
+        }
+        else if(count<2){
+            nums[++index] = nums[curr];
+            count++;
+        }
+    }
+    return index+1;
 }
 
 int main(){
     vector<int> nums = {1, 1, 1, 2, 2, 3};
     cout<<removeDuplicates(nums)<<endl;
+    print_vector<int>(nums);
     return 0;
 }
